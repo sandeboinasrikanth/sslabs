7214fd9bec97a9e84b5b621911c40dd9fdd015db
Sandeboina Srikanth <sandeboinasrikanth@gmail.com>
2023-07-14 18:01:02 +0530

    practice part

---
+++ src/sample.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+
+function Sample(){
+  return(
+    <div>
+      <h2>Sample component</h2>
+      <p>Sample Paragraph</p>
+    </div>
+  )
+}
+
+export default Sample;
+
+
+
+
+
+// create 2projects
